da5a581d5e33b9b13779df3f576a1bdedc1a5083
fatihint <fatihint@gmail.com>
2017-10-19 00:06:19 +0300

    header, footer ve dizin iskeleti olusturuldu.

---
+++ footer.php
@@ -0,0 +1,10 @@
+
+<footer>
+    <div id="footer">
+        
+    </div>
+</footer>
+
+</body>
+<script type="text/javascript" src="assets/js/bootstrap.js"> </script>
+</html>
---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <header id="header">
+            <div class="container">
+                <div class="col-xs-12 company-name">
+                    <h1>www.postasepeti.com</h1>
+                </div>
+                <div class="col-xs-12 menu">
+                    <ul class="list-unstyle list-inline">
+                        <li><a href="#">Ana Sayfa</a></li>
+                        <li><a href="#">Hakkımızda</a></li>
+                        <li><a href="#">Sistem</a></li>
+                        <li><a href="#">Deneme</a></li>
+                        <li><a href="#">İletişim</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="images-menu">
+                <div class="container">
+                    <ul class="list-unstyle list-inline">
+                        <li><button type="button" class="btn btn-primary btn-lg">Large button</button></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="after-header">
+                
+            </div>
+        </header>
