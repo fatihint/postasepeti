60e0ecab7bd53dfcff9b9710877f459510b5b8e4
Baran Ozoglu <abaranozoglu@gmail.com>
2017-10-19 22:54:31 +0300

    footer daki copyright yazısı ortalandı

--- footer.php
+++ footer.php
@@ -1,16 +1,11 @@
 
-<<<<<<< HEAD
 <footer>
     <div id="footer">
-      <div class="container" id="copyright">
+      <div class="container">
         <p>copyright grafik94.com 2017</p>
       </div>
     </div>
-=======
-<footer id="footer" class="text-center">
-    <p>copyright grafik94.com 2017</p>
->>>>>>> f5c2683e122316fb702b0a4fe876e7763c58e555
-</footer>
+
 
 </body>
 <script type="text/javascript" src="assets/js/bootstrap.js"> </script>
