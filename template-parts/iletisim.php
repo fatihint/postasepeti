9826bbe2f8c28c0db90dbf93620b704c8cce1cdd
Eren KARA <karaeren042@gmail.com>
2017-10-26 21:58:41 +0300

    iletisim sayfasi eklendi

---
+++ template-parts/iletisim.php
@@ -0,0 +1,108 @@
+<?php
+
+    include '../header.php';
+?>
+
+
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+          <style media="screen">
+            .row{
+              margin-bottom: 50px;
+            }
+            h1{
+              color:green;
+            }
+
+          </style>
+        </head>
+          <body>
+
+            <div class="jumbotron text-center">
+              <h1>Firmamızın Satış Temsilcileri</h1>
+            </div>
+
+            <div class="container">
+              <div class="row">
+                <div class="col-sm-4">
+                <h2>Eren KARA</h2 >
+                    <h4>TELEFON :05423898855</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="col-sm-4">
+                <h2>Fatih İNTEKİN</h2 >
+                  <h4>TELEFON :05423898855</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                     cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="col-sm-4">
+                  <h2>Ali Baran ÖZOĞLU</h2>
+                   <h4>TELEFON :05423898855</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                     cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-4">
+                <h2>Yasin Yağcıoğlu</h2 >
+                    <h4>TELEFON :05423898855</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="col-sm-4">
+                <h2>Nihat ÖZOĞLU</h2>
+                  <h4>TELEFON :05423898855</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                     cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="col-sm-4">
+                <h2>Ceyhun İNTEKİN</h2>
+                  <h4>TELEFON :05423898855</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                     cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+              </div>
+            </div>
+
+        </body>
+        </html>
+
+
+
+
+
+
+
+<?php
+
+    include '../footer.php';
+?>
