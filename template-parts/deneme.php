41586d3d7058bf41d9adb45bfcc267c10771b2b5
fatihint <fatihint@gmail.com>
2017-10-27 01:42:32 +0300

    deneme sayfasi olusturuldu

---
+++ template-parts/deneme.php
@@ -0,0 +1,27 @@
+<?php include '../header.php'; ?>
+
+<div id="deneme">
+
+    <div class="page-header text-center">
+        <h1>DENEME</h1>
+    </div>
+
+    <div class="container">
+        <h4>Sistemin nasıl çalıştığını test etmek için ücretsiz deneme mektubu yazabilirsiniz. Posta adresini bildiğiniz bir arkadaşınıza hitaben aşağıdaki boşlukları doldurarak, mektubu oluşturabilirsiniz.</h4>
+
+        <h2>Önce Kanal Seçiniz</h2>
+
+        <form class="" action="index.html" method="post">
+            <span class="radio-span"> <label for="radio1">1</label> <input type="radio" name="radio1"> </span>
+            <span class="radio-span"> <label for="radio2">2</label> <input type="radio" name="radio2"> </span>
+            <span class="radio-span"> <label for="radio3">3</label> <input type="radio" name="radio3"> </span>
+            <span class="radio-span"> <label for="radio4">4</label> <input type="radio" name="radio4"> </span>
+            <span class="radio-span"> <label for="radio5">5</label> <input type="radio" name="radio5"> </span>
+            <span class="radio-span"> <label for="radio6">6</label> <input type="radio" name="radio6"> </span>
+        </form>
+    </div>
+
+</div>
+
+
+<?php include '../footer.php'; ?>
