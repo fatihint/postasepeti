3ebb19cdeb5a60de20beb1921f827f80b6277afa
Eren KARA <karaeren042@gmail.com>
2017-12-02 23:30:09 +0300

    İletisim sayfası revize edildi

--- iletisim.php
+++ iletisim.php
@@ -5,44 +5,47 @@
         <h1>FİRMAMIZIN SATIŞ TEMSİLCİLERİ</h1>
     </div>
     <div class="container text-center">
+
         <div class="row">
-            <div class="col-sm-4">
-                <h2>Mahmut Yıldız</h2>
-                <h5>Genel Koordinatör</h5>
-                <h4>TELEFON :0536 565 54 66</h4>
+          <div class="col-sm-8" id="map">
+            <h1>Harita</h1>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1528.332643324943!2d32.74982824565179!3d39.99358473574594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34a27cb614873%3A0xd01c63509a39cd11!2sSerhat+Mahallesi%2C+Melih+G%C3%B6k%C3%A7ek+Blv.+No%3A31+D%3A18%2C+06378+%C4%B0vedik+Osb%2FYenimahalle%2FAnkara!5e0!3m2!1str!2str!4v1512243392636" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+          </div>
+          <div class="col-sm-4">
+            <div>
+                <h3>Mahmut Yıldız</h3>
+                <h4>Genel Koordinatör - Kanal 1</h4>
+                <h5>TELEFON :0536 565 54 66</h5>
                 <p>mahmut.yildiz@oekopost.com</p>
             </div>
-            <div class="col-sm-4">
-                <h2>Adil Bayalan</h2>
-                <h5>Almanya Koordinatör</h5>
-                <h4>TELEFON :0538 887 09 95</h4>
-                <p>mahmut.yildiz@oekopost.com</p>
+            <div>
+                <h3>Adil Bayalan</h3>
+                <h4>Almanya Koordinatör - Kanal 2</h4>
+                <h5>TELEFON :0538 887 09 95</h5>
+                <p>adil.bayalan @oekopost.com</p>
             </div>
-            <div class="col-sm-4">
-                <h2>Nihat Özoğlu</h2>
-                <h5>Müşteri Temsilcisi</h5>
-                <h4>TELEFON :0505 543 00 94</h4>
+            <div >
+                <h3>Nihat Özoğlu</h3>
+                <h4>Müşteri Temsilcisi - Kanal 3</h4>
+                <h5>TELEFON :0505 543 00 94</h5>
                 <p>grafik94@gmail.com</p>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-4">
-                <h2>Mahmut Diker</h2 >
-                <h5>Müşteri Temsilcisi</h5>
-                <h4>TELEFON :0505 580 62 10</h4>
+
+            <div >
+                <h3>Mahmut Diker</h3>
+                <h4>Müşteri Temsilcisi - Kanal 4</h4>
+                <h5>TELEFON :0505 580 62 10</h5>
                 <p>mahmut.diker@oekopost.com</p>
             </div>
-            <div class="col-sm-4">
-                <h2>Volkan Yiğit</h2>
-                <h5>Müşteri Temsilcisi</h5>
-                <h4>TELEFON :0532 277 51 21</h4>
+            <div >
+                <h3>Volkan Yiğit</h3>
+                <h4>Müşteri Temsilcisi- Kanal 5</h4>
+                <h5>TELEFON :0532 277 51 21</h5>
                 <p>volkan.yigit@oekopost.com</p>
             </div>
-            <div class="col-sm-4">
+          </div>
 
-            </div>
-        </div>
-    </div>
+      </div>
 </div>
 
 <?php include 'footer.php'; ?>
